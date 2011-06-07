9d4ac0d5b181a56b8c3e9a6822d5ded58f0b969d
LuisFernandoMarques <lfm.evolution@gmail.com>
2011-06-06 22:30:37 -0300

    Implementação da Interface.

--- src/Program.java
+++ src/Program.java
@@ -1,18 +1,20 @@
 import javax.swing.JOptionPane;
 
-
 public class Program {
 
+	private static Caixas _caixas;
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) 
 	{
-		StringBuilder menu = new StringBuilder();
-		int opcaoMenu = 0; 
+		int opcaoMenu = 0;
+		_caixas = new Caixas();
 		
 		do
 		{
+			StringBuilder menu = new StringBuilder();
 			menu.append("GERENCIADOR DE CAIXAS");
 			menu.append("1 - Inserir novo cliente no caixa");
 			menu.append("2 - Imprimir Tempo Maximo Atendimento");
@@ -27,28 +29,104 @@ public class Program {
 			switch(opcaoMenu)
 			{
 				case 1: //inserir novo cliente
-					
+					InserirCliente();
 					break;
 				
 				case 2:  //imprimir tempo máximo de atendimento
-					
+					ImprimirTempoMaximoAtendimento();
 					break;
 					
 				case 3: //imprimi as informações de todos os caixas
-					
+					ImprimirInformacoesCaixas();
 					break;
 					
 				case 4: //atender cliente
+					AtenderCliente();
+					break;
 					
-					break;	
-				case 0: // sair
+				case 0: //sair
+					JOptionPane.showMessageDialog(null, "Programa Encerrado!");
 					break;
 					
 				default:
+					JOptionPane.showMessageDialog(null, "Opção Inválida!");
 					break;
-			}		
-		
+			}
 		}while(opcaoMenu != 0);
 	}
-
+	
+	public static boolean InserirCliente()
+	{
+		int cliente = 0;
+		boolean retorno = false;
+		
+		try
+		{
+			cliente = Integer.getInteger(JOptionPane.showInputDialog("Informe o tempo de atendimento do cliente"));
+			if(cliente == 0 || cliente < 0)
+			{
+				JOptionPane.showMessageDialog(null, "O tempo de atendimento não é válido!");
+			}
+			else
+			{
+				_caixas.AdicionarCliente(cliente);
+				retorno = true;
+			}
+		}
+		catch(Exception ex)
+		{		
+			TratarErro("Erro ao inserir o cliente!", ex);
+		}
+		return retorno;
+	}
+	
+	public static boolean ImprimirTempoMaximoAtendimento()
+	{
+		boolean retorno = false;
+		try
+		{
+			retorno = true;
+		}
+		catch(Exception ex)
+		{
+			TratarErro("Erro ao imprimir o tempo máximo de atendimento!", ex);
+		}
+		return retorno;
+	}
+	
+	public static boolean ImprimirInformacoesCaixas()
+	{
+		boolean retorno = false;
+		try
+		{
+			
+			retorno = true;
+		}
+		catch(Exception ex)
+		{
+			TratarErro("Erro ao imprimir as informações dos caixas!", ex);
+		}
+		return retorno;
+	}
+	
+	public static boolean AtenderCliente()
+	{
+		boolean retorno = false;
+		try
+		{
+		   
+			retorno = true;
+		}
+		catch(Exception ex)
+		{
+			TratarErro("Erro ao imprimir as informações dos caixas!", ex);
+		}
+		return retorno;
+	}
+	
+	public static void TratarErro(String message, Exception ex)
+	{
+		JOptionPane.showMessageDialog(null, String.format("{0} \n Erro: {1}", message, ex.getMessage()));
+	}
+	
 }

7e9ebef03d0e4605a904957132176af37f48e343
LuisFernandoMarques <lfm.evolution@gmail.com>
2011-06-06 21:33:36 -0300

    Alterações na Interface

--- src/Program.java
+++ src/Program.java
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane;
+
 
 public class Program {
 
@@ -6,6 +8,40 @@ public class Program {
 	 */
 	public static void main(String[] args) 
 	{
+		StringBuilder menu = new StringBuilder();
+		int opcaoMenu = 0; 
+		
+		menu.append("GERENCIADOR DE CAIXAS");
+		menu.append("1 - Inserir novo cliente no caixa");
+		menu.append("2 - Imprimir Tempo Maximo Atendimento");
+		menu.append("3 - Informacoes dos caixas");
+		menu.append("4 - Atender Cliente");
+		menu.append("0 - Sair");
+		menu.append("_______________________________________");
+		menu.append("Digite a opcao desejada: ");
+		
+		opcaoMenu = Integer.getInteger(JOptionPane.showInputDialog(menu.toString()));
+		
+		switch(opcaoMenu)
+		{
+			case 1:
+				break;
+			
+			case 2:
+				break;
+				
+			case 3:
+				break;
+				
+			case 4:
+				break;
+		
+		
+		}
+		
+		
+		
+		
 		
 		
 		
